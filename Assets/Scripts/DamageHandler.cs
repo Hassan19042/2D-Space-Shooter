cffb3314ab721996610eaf162a7a511bfbf2a7ad
Hassan19042 <35747021+Hassan19042@users.noreply.github.com>
2018-01-23 22:40:55 +0000

    Initial commit

---
+++ Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+    public float invulnPeriod = 0;
+    public int health = 1;
+    float invulnTimer = 0;
+    int correctLayer;
+    float invulnAnimTimer = 0;
+
+    SpriteRenderer spriteRend;
+
+    private void Start()
+    {
+        correctLayer = gameObject.layer;
+
+        //NOTE This will only get the renderer on the parent object, for example it doesn't work for children objects i.e "Enemy01"
+        spriteRend = GetComponent<SpriteRenderer>();
+
+        if (spriteRend == null)
+        {
+            spriteRend = transform.GetComponentInChildren<SpriteRenderer>();
+
+            if (spriteRend == null)
+            {
+                Debug.LogError("Object '" + gameObject.name + "' has no sprite renderer.");
+            }
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        health--;
+
+        if (invulnPeriod > 0)
+        {
+            invulnTimer = invulnPeriod;
+            gameObject.layer = 10;
+        }
+    }
+
+    void Update()
+    {
+
+        if (invulnTimer > 0)
+        {
+            invulnTimer -= Time.deltaTime;
+
+            if (invulnTimer <= 0)
+            {
+                gameObject.layer = correctLayer;
+                if (spriteRend != null)
+                {
+                    spriteRend.enabled = true;
+                }
+            }
+            else
+            {
+                if (spriteRend != null)
+                {
+                    spriteRend.enabled = !spriteRend.enabled;
+                }
+            }
+        }
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    public float rotSpeed = 90f;
+
+    Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            //Find the player's ship!
+            GameObject go = GameObject.FindWithTag("Player");
+
+            if (go != null)
+            {
+                player = go.transform;
+            }
+        }
+
+        //At this point, the player has been found or they do not exist right now
+        if (player == null)
+            return; //try again on the next frame!
+
+        //HERE -- we have definitely found a player. Turn to face it!
+
+        Vector3 dir = player.position - transform.position;
+        dir.Normalize();
+
+        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+        Quaternion desiredRot = Quaternion.Euler(0, 0, zAngle);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+    }
+}
+
